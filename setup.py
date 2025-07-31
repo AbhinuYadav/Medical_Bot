f45797f0ddcff763cc012f02cbbcb5e80b313a3b
AbhinuYadav <abhinuyadav@gmail.com>
2025-07-31 18:12:41 +0530

    Working

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical_BOT",
+    version="0.1.0",
+    author="Abhinu Yadav",
+    author_email="abhinuyadav@gmail.com",
+    packages=find_packages(),
+    intall_requires=[]
+)
